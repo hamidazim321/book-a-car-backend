76e745fe3e131ab1e92d0a35da556b869402bf8e
Xghayor <cc.baqir@gmail.com>
2024-02-16 00:27:47 +0100

    update sessions controller

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -5,16 +5,17 @@ class Users::SessionsController < Devise::SessionsController
 
   private
 
+  def respond_with(current_user, _opts = {})
+    render json: {
+      status: {
+        code: 200,
+        message: 'Logged in successfully.',
+        data: { user: UserSerializer.new(current_user).serializable_hash[:data][:attributes] }
+      }
+    }, status: :ok
+  end
+
   def respond_to_on_destroy
-    if request.headers['Authorization'].present?
-      token = request.headers['Authorization'].split(' ').last
-      jwt_payload = decode_jwt(token)
-
-      if jwt_payload.present? && jwt_payload.key?('jti')
-        current_user = User.find_by(jti: jwt_payload['jti'])
-      end
-    end
-
     if current_user
       render json: {
         status: 200,
@@ -25,16 +26,6 @@ class Users::SessionsController < Devise::SessionsController
         status: 401,
         message: "Couldn't find an active session."
       }, status: :unauthorized
-    end
-  end
-
-  def decode_jwt(token)
-    begin
-      JWT.decode(token, Rails.application.credentials.secret_key_base, true, algorithm: 'HS256').first
-    rescue JWT::DecodeError => e
-      # Handle the error, e.g., log it or return nil
-      Rails.logger.error("JWT Decode Error: #{e.message}")
-      nil
     end
   end
 end
